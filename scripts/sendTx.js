8a017f7e68bea70e5cce628307e240a6c54b968e
Marcel Schmitt <schmitt.marcel97@gmail.com>
2021-06-07 22:59:34 +0200

    Test transaction to smart contract

---
+++ scripts/sendTx.js
@@ -0,0 +1,31 @@
+// does not work because smart contract does not support receiving ether
+
+async function main() {
+    require('dotenv').config();
+    const { API_URL, PRIVATE_KEY } = process.env;
+    const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+    const web3 = createAlchemyWeb3(API_URL);
+    const myAddress = '0x89f9Be678ab98045Df0c8FA10E4978FAb0cadB30' //TODO: replace this address with your own public address
+   
+    const nonce = await web3.eth.getTransactionCount(myAddress, 'latest'); // nonce starts counting from 0
+
+    const transaction = {
+     'to': '0x739af7A68f102A7E76A746afc5Df13Dd0c8911AC', // faucet address to return eth
+     'value': 1000000000000000, // 1 ETH
+     'gas': 6000000, 
+     'nonce': nonce,
+     // optional data field to send message or execute smart contract
+    };
+   
+    const signedTx = await web3.eth.accounts.signTransaction(transaction, PRIVATE_KEY);
+    
+    web3.eth.sendSignedTransaction(signedTx.rawTransaction, function(error, hash) {
+    if (!error) {
+      console.log("🎉 The hash of your transaction is: ", hash, "\n Check Alchemy's Mempool to view the status of your transaction!");
+    } else {
+      console.log("❗Something went wrong while submitting your transaction:", error)
+    }
+   });
+}
+
+main();
